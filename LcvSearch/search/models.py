021af1bf3bde2c240adeb69aef6b7fb071f36bcd
Wingchi Mak <makwin9chi@gmail.com>
2020-05-05 00:31:03 -0400

    modify elasticsearch data model

--- LcvSearch/search/models.py
+++ LcvSearch/search/models.py
@@ -1,41 +1,22 @@
-from elasticsearch_dsl import DocType, Completion, Keyword, Text
-from elasticsearch_dsl.analysis import CustomAnalyzer as _CustomAnalyzer
+from elasticsearch_dsl import DocType, Keyword, Text, Date
 from elasticsearch_dsl.connections import connections
 
 connections.create_connection(hosts=["localhost"])
 
-class CustomAnalyzer(_CustomAnalyzer):
-    def get_analysis_definition(self):
-        return {}
-
-
-ik_analyzer = CustomAnalyzer("ik_max_word", filter=["lowercase"])
-
 
 class CommonType(DocType):
-    # Common doc type for Amazon, TechCrunch and Zhihu
-    suggest = Completion(analyzer=ik_analyzer)
-    title = Text()
-    url = Keyword()
-    content = Text()
     source = Keyword()
+    author = Keyword()
+    title = Text()
+    description = Text()
+    content = Text()
+    url = Keyword()
+    urlToImage = Keyword()
+    publishedAt = Date()
 
-    class Meta:
-        index = "data"
-
-
-class AmazonType(DocType):
-    class Meta:
-        index = "data"
-        doc_type = "amazon"
-
-
-class TechcrunchType(DocType):
-    class Meta:
-        index = "data"
-        doc_type = "techcrunch"
+    class Index:
+        name = "news"
 
 
 if __name__ == "__main__":
-    AmazonType.init()
-    TechcrunchType.init()
+    CommonType.init()
